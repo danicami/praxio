9a203e05b1d1fd53134d41e0d5b6f65ec24e6a5c
Daniela <danicami@gmail.com>
2019-01-20 22:41:43 -0200

    Modelo de dados

---
+++ Webapi_Praxio/Scripts/Clientes.sql
@@ -0,0 +1,25 @@
+﻿USE [Praxio]
+GO
+
+/****** Object:  Table [dbo].[Clientes]    Script Date: 20/01/2019 22:36:31 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Clientes](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](50) NULL,
+	[Email] [varchar](50) NULL,
+	[Endereco] [varchar](50) NULL,
+	[Cidade] [varchar](50) NULL,
+	[Bairro] [varchar](50) NULL,
+	[Cep] [int] NULL,
+	[Estado] [varchar](50) NULL,
+ CONSTRAINT [PK_Clientes] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
---
+++ Webapi_Praxio/Scripts/Users.sql
@@ -0,0 +1,29 @@
+﻿
+USE [Praxio]
+GO
+
+/****** Object:  Table [dbo].[Users]    Script Date: 20/01/2019 22:38:19 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[ID] [nvarchar](20) NOT NULL,
+	[ClientSecret] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+INSERT INTO dbo.Users
+           (ID
+           ,ClientSecret)
+     VALUES
+           ('usuario01'
+           ,'94be650011cf412ca906fc335f615cdc')
+GO
